64cd59eb6e1d4b500a39433d1cb339d444d1c1d6
Shivansh- Btgr <shivanshbtgr25@gmail.com>
2024-03-27 15:18:43 +0000

    Completed

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 def calculate(list):
+    calculations = {}
 
-
-
+    if len(list) < 9 :
+        raise ValueError("List must contain nine numbers.")
+    
+    data = np.array([list[0:3],list[3:6],list[6:9]])
+    #I didnt use reshape because I didnt know if I would receive a numpy array or a regular one
+    calculations['mean'] = [data.mean(axis=0), data.mean(axis=1), np.mean(list)]
+    calculations['varience'] = [data.var(axis=0), data.var(axis=1), np.var(list)]
+    calculations['standard deviation'] = [data.std(axis=0), data.std(axis=1), np.std(list)]
+    calculations['max'] = [data.max(axis=0), data.max(axis=1), np.max(list)]
+    calculations['min'] = [data.min(axis=0), data.min(axis=1), np.min(list)]
+    calculations['sum'] = [data.sum(axis=0), data.sum(axis=1), np.sum(list)]
 
     return calculations
